f8a61f0f45a891be647bfbe2267d3b5ed87d302a
HaManhDong <dongha.27995@gmail.com>
2017-07-25 02:20:05 +0700

    fix wtf form for page manage farmer

--- pgscm/farmer/views.py
+++ pgscm/farmer/views.py
@@ -2,9 +2,11 @@ from flask import render_template, current_app
 from flask_security import roles_accepted, current_user
 
 from . import farmer
+from .forms import FarmerForm
 
 from pgscm.db import models
 from pgscm import const as c
+from pgscm.utils import __
 
 
 @farmer.route('/vi/nong-dan', endpoint='index_vi')
@@ -15,12 +17,15 @@ def index():
         return render_template('farmer/index.html')
     else:
         province_id = current_user.province_id
+        form = FarmerForm()
+        form.gender.choices = [(c.GenderType.male, __('Male')), (c.GenderType.female, __('Female'))]
+        form.type.choices = [(c.FarmerType.member, __('Member')), (c.FarmerType.reviewer, __('Reviewer'))]
+        form.group.choices = [(p.id, p.name) for p in
+                              models.Group.query.all()]
         if province_id:
-            # groups = models.Group.query.filter_by(
-            #     province_id=province_id).all()
             farmers = models.Farmer.query.join(models.Group).filter(models.Group.query.filter_by(
                 province_id=province_id).all())
         else:
             farmers = models.Farmer.query.all()
         return render_template('farmer/index.html', farmers=farmers,
-                               enabled_action=['edit', 'delete', 'add'])
+                               form=form)
