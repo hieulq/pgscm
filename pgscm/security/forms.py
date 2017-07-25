992ee49735737152c0297d2108646a52c25544f5
Hieu LE <hieulq@vn.fujitsu.com>
2017-07-25 15:15:43 +0700

    reformat macro and select field display

--- pgscm/security/forms.py
+++ pgscm/security/forms.py
@@ -1,9 +1,21 @@
 from flask_security import forms
+from wtforms import SelectField, validators
+
 from pgscm.utils import __
+from pgscm.db import models
 
 
 class RegisterForm(forms.RegisterForm):
     fullname = forms.StringField(__('Full name'))
+    province_id = SelectField(
+        __('Province'),
+        validators=[validators.DataRequired(message=__('Required field!'))])
+
+    def __init__(self, *args, **kwargs):
+        super(RegisterForm, self).__init__(*args, **kwargs)
+        self.province_id.choices = [(p.province_id, p.name) for p in
+                                    models.Province.query.order_by(
+                                        models.Province.name).all()]
 
 
 class LoginForm(forms.LoginForm):
