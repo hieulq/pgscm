f7ef8c7dee0451b69938c2ea28a1dca07fecdf1f
Hieu LE <hieulq@vn.fujitsu.com>
2017-07-26 15:39:26 +0700

    fix css of select2

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = \
-        'mysql+pymysql://root@localhost:3306/pgscm_dev'
+        'mysql+pymysql://pgs:pgscm@localhost:3306/pgscm_dev'
 
 
 class TestingConfig(Config):
