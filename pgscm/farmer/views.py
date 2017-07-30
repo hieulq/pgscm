7ee11ef50796899987f3c1fd293afbcb06fb402e
HaManhDong <dongha.27995@gmail.com>
2017-07-30 22:06:33 +0700

    add check role for action crud in page manage farmer

--- pgscm/farmer/views.py
+++ pgscm/farmer/views.py
@@ -11,6 +11,8 @@ from pgscm.db import models
 from pgscm import const as c
 from pgscm.utils import __, DeleteForm
 
+crud_role = c.ADMIN_MOD_ROLE
+
 
 @farmer.route('/vi/nong-dan', endpoint='index_vi', methods=['GET', 'POST'])
 @farmer.route('/en/farmer', endpoint='index_en', methods=['GET', 'POST'])
@@ -41,55 +43,67 @@ def index():
                                      models.Group.query.order_by(
                                          models.Group.name.asc()).all()]
 
-        if request.method == 'POST' and form.data['submit'] \
-                and form.validate_on_submit():
-            # edit user
-            if form.id.data:
-                edit_farmer = sqla.session.query(models.Farmer) \
-                    .filter_by(id=form.id.data).one()
-                edit_farmer.farmer_code = form.farmer_code.data
-                edit_farmer.name = form.name.data
-                edit_farmer.gender = form.gender.data
-                edit_farmer.type = form.type.data
-                if edit_farmer.group_id != form.group_id.data:
-                    new_group = models.Group.query.filter_by(
+        # form create or edit submit
+        if request.method == 'POST' and form.data['submit']:
+            if not check_role():
+                return redirect(url_for(request.endpoint))
+            elif form.validate_on_submit():
+                # edit user
+                if form.id.data:
+                    edit_farmer = sqla.session.query(models.Farmer) \
+                        .filter_by(id=form.id.data).one()
+                    edit_farmer.farmer_code = form.farmer_code.data
+                    edit_farmer.name = form.name.data
+                    edit_farmer.gender = form.gender.data
+                    edit_farmer.type = form.type.data
+                    if edit_farmer.group_id != form.group_id.data:
+                        new_group = models.Group.query.filter_by(
+                            id=form.group_id.data).one()
+                        edit_farmer.group = new_group
+                    sqla.session.commit()
+                    for fm in farmers:
+                        if fm.id == edit_farmer.id:
+                            farmers.remove(fm)
+                            farmers.append(edit_farmer)
+                    flash('Update farmer success!', 'info')
+
+                # add user
+                else:
+                    group_farmer = models.Group.query.filter_by(
                         id=form.group_id.data).one()
-                    edit_farmer.group = new_group
+                    new_farmer = models.Farmer(type=form.type.data,
+                        farmer_code=form.farmer_code.data, name=form.name.data,
+                        group=group_farmer, gender=form.gender.data)
+                    sqla.session.add(new_farmer)
+                    sqla.session.commit()
+                    farmers.append(new_farmer)
+                    flash('Add farmer success!', 'info')
+
+                return redirect(url_for(request.endpoint))
+
+        # form delete submit
+        if request.method == 'POST' and dform.data['submit_del']:
+            if not check_role():
+                return redirect(url_for(request.endpoint))
+            elif dform.validate_on_submit():
+                del_farmer = sqla.session.query(models.Farmer) \
+                    .filter_by(id=dform.id.data).one()
+                del_farmer._deleted_at = func.now()
+                if dform.modify_info.data:
+                    del_farmer._modify_info = dform.modify_info.data
                 sqla.session.commit()
-                for fm in farmers:
-                    if fm.id == edit_farmer.id:
-                        farmers.remove(fm)
-                        farmers.append(edit_farmer)
-                flash('Update farmer success!', 'info')
+                farmers.remove(del_farmer)
+                flash('Delete farmer success!', 'info')
 
-            # add user
-            else:
-                group_farmer = models.Group.query.filter_by(
-                    id=form.group_id.data).one()
-                new_farmer = models.Farmer(farmer_code=form.farmer_code.data,
-                                           name=form.name.data,
-                                           group=group_farmer,
-                                           gender=form.gender.data,
-                                           type=form.type.data)
-                sqla.session.add(new_farmer)
-                sqla.session.commit()
-                farmers.append(new_farmer)
-                flash('Add farmer success!', 'info')
-
-            return redirect(url_for(request.endpoint))
-
-        if request.method == 'POST' and dform.data['submit_del'] \
-                and dform.validate_on_submit():
-            del_farmer = sqla.session.query(models.Farmer) \
-                .filter_by(id=dform.id.data).one()
-            del_farmer._deleted_at = func.now()
-            if dform.modify_info.data:
-                del_farmer._modify_info = dform.modify_info.data
-            sqla.session.commit()
-            farmers.remove(del_farmer)
-            flash('Delete farmer success!', 'info')
-
-            return redirect(url_for(request.endpoint))
+                return redirect(url_for(request.endpoint))
 
         return render_template('farmer/index.html', farmers=farmers,
                                form=form, dform=dform)
+
+
+def check_role():
+    for r in crud_role:
+        if r == current_user.roles[0].name:
+            return True
+    flash('You have no permission!', 'warning')
+    return False
